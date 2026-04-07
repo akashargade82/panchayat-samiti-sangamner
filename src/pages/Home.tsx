a5eb1a197c59f58e694f9ad0656465bb8ae9249c
akashargade82 <akashargade82@gmail.com>
2026-04-07 14:20:09 +0530

    testing changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -33,7 +33,7 @@ const STATIC_LEADERS = [
   { name: "मा.श्री. जयकुमार गोरे", role: "मंत्री, ग्रामविकास व पंचायतराज विभाग", image: "https://cdnbbsr.s3waas.gov.in/s3e6c2dc3dee4a51dcec3a876aa2339a78/uploads/2024/12/20241226862517803.png" },
   { name: "मा.श्री. एकनाथ डवले", role: "प्रधान सचिव, ग्रामविकास विभाग", image: "https://cdnbbsr.s3waas.gov.in/s3e6c2dc3dee4a51dcec3a876aa2339a78/uploads/2024/12/202412261951170016.png" },
   { name: "मा.श्री.आनंद भंडारी ( भा.प्र.से )", role: "मुख्य कार्यकारी अधिकारी , जिल्हा परिषद अहिल्यानगर", image: "https://cdnbbsr.s3waas.gov.in/s385b75d04f478d3841e38eb64aefdb05a/uploads/2025/07/20250728469510874.jpg" },
-  { name: "मा.ना.श्री प्रविण अण्णासाहेब सिनारे", role: "गट विकास अधिकारी (उ.श्रे.), पंचायत समिती संगमनेर", image: bdoImage },
+  { name: "मा.श्री. प्रविण अण्णासाहेब सिनारे", role: "गट विकास अधिकारी (उ.श्रे.), पंचायत समिती संगमनेर", image: bdoImage },
 ];
 
 const bannerSlides = [
